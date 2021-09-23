e48793d30f176c48da88e8f9b05224ae0993c301
Steven Abadie <steven@anothernewthing.com>
2021-09-22 20:27:39 -0600

    Move resolve and block handlers to separate apiHelper module
    This also fixes an error in the blockHandler function. The SWR mutate
    function was not being passed to the function.

--- components/Report.js
+++ components/Report.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
+import { blockHandler, resolveHandler } from "lib/apiHelper";
 
-const Report = ({ report, blockHandler, resolveHandler, mutate }) => (
+const Report = ({ report, mutate }) => (
   <div className="grid grid-cols-3 gap-4 border-2 rounded-lg p-4">
     <div>id: {report.id.substr(-12)}</div>
     <div>Type: {report.reportType}</div>
--- components/ReportList.js
+++ components/ReportList.js
@@ -1,24 +1,6 @@
 import useSWR, { useSWRConfig } from "swr";
 import Report from "./Report";
 
-const blockHandler = (id) => {
-  fetch(["/api/reports/", id].join(""), {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ blockState: true }),
-  });
-  mutate("/api/reports");
-};
-
-const resolveHandler = (id, { mutate }) => {
-  fetch(["/api/reports/", id].join(""), {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ reportState: "CLOSED" }),
-  });
-  mutate("/api/reports");
-};
-
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const ReportList = () => {
@@ -38,13 +20,7 @@ const ReportList = () => {
       {!data && <div>loading...</div>}
       {data &&
         data.map((report) => (
-          <Report
-            key={report.id}
-            report={report}
-            resolveHandler={resolveHandler}
-            blockHandler={blockHandler}
-            mutate={mutate}
-          />
+          <Report key={report.id} report={report} mutate={mutate} />
         ))}
     </div>
   );
