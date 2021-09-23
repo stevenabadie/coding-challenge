5b83d75240c8c0fc02300c10abebe778b84a1692
Steven Abadie <steven@anothernewthing.com>
2021-09-22 20:04:17 -0600

    Modify database seed function to allow for larger seed data set

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,28 +1,28 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
-const data = require('../data/reports')
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const data = require("../data/reports");
 
 async function main(data) {
   // This doesn't currently output errors from individual loops in the data creation. It may be
   // worth adding if there is a need for a larger import of data.
-  Promise.all(data.elements.map(async(report) => {
+  for (const report of data.elements) {
     await prisma.report.create({
       data: {
         id: report.id,
         reportType: report.payload.reportType,
         reportState: report.state,
         message: report.payload.message,
-        createdAt: report.created
-      }
-    })
-  }))
+        createdAt: report.created,
+      },
+    });
+  }
 }
 
 main(data)
   .catch((e) => {
-    console.error(e)
-    process.exit(1)
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
